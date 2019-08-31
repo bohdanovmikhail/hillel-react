4c6de2b94f87fcbc7adf4e5f35b7497dfbd89028
BilenkoDenis <53963998+BilenkoDenis@users.noreply.github.com>
2019-08-31 13:59:48 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,94 @@ import AddEditForm from './pages/AddEditForm';
 
 import './App.css';
 
+const LOCAL_STORAGE_KEYS = {
+  CONTACTS: 'contacts',
+};
 
-export default function App() {
-  const contactList = [
-    {
-      fio: 'Qwerty',
-    },
-    {
-      fio: 'Asdfgh',
-    },
-  ];
 
-  return (
-    <Router>
-      <div>
-        <NavLink
-          to="/"
-          exact
-          activeClassName="active"
-        >
-            Main page
-        </NavLink>
-        <NavLink
-          to="/add"
-          activeClassName="active"
-        >
-          Add new record
-        </NavLink>
-      </div>
+export default class App extends React.Component {
+  state = {
+    contacts: [],
+    isContacts: false,
+  };
 
-      <Route path="/" exact render={() => <MainPage contacts={contactList} />} />
-      <Route path="/add" component={AddEditForm} />
-      <Route path="/edit/:recordId" render={({ match }) => <AddEditForm match={match} contacts={contactList} />} />
-    </Router>
-  );
+  componentWillMount() {
+    this.getData();
+  }
+
+  getData() {
+    const str = localStorage.getItem(LOCAL_STORAGE_KEYS.CONTACTS);
+    let data = str ? JSON.parse(str) : [];
+
+    this.setState({
+      contacts: data
+    });
+  }
+
+  setData(contacts) {
+    const str = JSON.stringify(contacts);
+    localStorage.setItem(LOCAL_STORAGE_KEYS.CONTACTS, str);
+  }
+
+  onContactSave(contact) {
+    const { contacts } = this.state;
+
+    contacts.push(contact);
+    this.setData(contacts);
+    this.getData();
+  }
+
+  updateContact(contact, id) {
+    const {contacts} = this.state;
+
+    contacts[id] = contact;
+
+    this.setData(contacts);
+    this.getData();
+  }
+
+  deleteContact(id) {
+    const {contacts} = this.state;
+
+    contacts.splice(id, 1);
+    this.setData(contacts);
+    this.getData();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="Navigation">
+          <NavLink
+              to="/"
+              exact
+              className="main-icon-active"
+          >
+          </NavLink>
+          <NavLink
+              to="/add"
+              className="add-icon-active"
+          >
+          </NavLink>
+        </div>
+
+        <Route path="/"
+               exact render={() =>
+                   <MainPage
+                             onDeleteContact={id => this.deleteContact(id)}
+                             contacts={this.state.contacts}
+                   />}
+        />
+        <Route path="/add" render={() => <AddEditForm onDataSave={contact => this.onContactSave(contact)}/>}/>
+        <Route path="/edit/:recordId"
+               render={({match}) =>
+                   <AddEditForm
+                       match={match}
+                       contacts={this.state.contacts}
+                       onUpdateContact={(contact, id) => this.updateContact(contact, id)}
+                   />}
+        />
+      </Router>
+    );
+  }
 }
